e464d851840287ee12528241c858314658e284fa
KalilovM <57083458+KalilovM@users.noreply.github.com>
2022-11-23 16:32:31 +0600

    Created Path and rename function to rename uploaded images
    Configured media and static paths
    
    Added JWT authorization
    
    Created swagger ui

---
+++ core/helpers.py
@@ -0,0 +1,33 @@
+import os
+import uuid
+from datetime import datetime
+from django.utils.deconstruct import deconstructible
+from rest_framework import serializers
+
+
+@deconstructible
+class PathAndRename:
+    def __init__(self, sub_path):
+        self.sub_path = sub_path
+
+    def __call__(self, instance, filename):
+        ext = filename.split(".")[-1]
+        filename = f"{uuid.uuid4().hex}.{ext}"
+
+        def make_path(path):
+            now = datetime.now()
+            return os.path.join(path, f"{now.year}/{now.month}")
+
+        date_path = make_path(self.sub_path)
+        full_path = os.path.join(date_path, filename)
+
+        return full_path
+
+
+class ImageUrlField(serializers.RelatedField):
+    def to_representation(self, value):
+        url = value.image.url
+        request = self.context.get("request", None)
+        if request is not None:
+            return request.build_absolute_uri(url)
+        return url
--- core/urls.py
+++ core/urls.py
@@ -1,21 +1,49 @@
-"""top URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+schema_view = get_schema_view(
+    openapi.Info(
+        title="TopShopes API",
+        default_version="v1",
+        description="It's api created just for convinent using this api routes",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
+
+urlpatterns = (
+    [
+        # swagger
+        re_path(
+            r"^api(?P<format>\.json|\.yaml)$",
+            schema_view.without_ui(cache_timeout=0),
+            name="schema-json",
+        ),
+        re_path(
+            r"^api/$",
+            schema_view.with_ui("swagger", cache_timeout=0),
+            name="schema-swagger-ui",
+        ),
+        re_path(
+            r"^redoc/$",
+            schema_view.with_ui("redoc", cache_timeout=0),
+            name="schema-redoc",
+        ),
+        path("admin/", admin.site.urls),
+        path("api/users/", include("users.urls"), name="users_base_API"),
+        path("api/shops/", include("shops.urls"), name="shops_base_API"),
+        path("api/token/create/", TokenObtainPairView.as_view(), name="token_create"),
+        path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    ]
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
