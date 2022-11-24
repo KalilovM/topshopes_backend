7a2e33b971febb11dddd105e585cb5a194acb755
KalilovM <57083458+KalilovM@users.noreply.github.com>
2022-11-24 15:56:17 +0600

    Changed order fields to decimals
    Added logic to autocount product prices

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+from typing import Tuple, Dict
 import uuid
 from django.db import models
 from core.helpers import PathAndRename
@@ -6,12 +8,33 @@ from core.helpers import PathAndRename
 class OrderItem(models.Model):
     product_image = models.ImageField(upload_to=PathAndRename("orders/items/product/"))
     product_name = models.CharField(max_length=100)
-    product_price = models.PositiveIntegerField()
+    product_price = models.DecimalField(max_digits=10, decimal_places=2)
     product_quantity = models.PositiveIntegerField()
     order = models.ForeignKey("Order", on_delete=models.CASCADE, related_name="items")
 
     def __str__(self):
         return self.product_name
+
+    def change_order_total(self, isAdd: bool) -> None:
+        total_price = self.product_price * Decimal(self.product_quantity)
+        order = Order.objects.get(id=self.order.id)
+        total_price -= total_price * Decimal(order.discount) / 100
+
+        if isAdd:
+            order.total_price += total_price
+        else:
+            order.total_price -= total_price
+        order.total_price += total_price * Decimal(order.tax) / 100
+
+        order.save()
+
+    def save(self, *args, **kwargs) -> None:
+        self.change_order_total(True)
+        return super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs) -> Tuple[int, Dict[str, int]]:
+        self.change_order_total(False)
+        return super().delete(*args, **kwargs)
 
 
 class Order(models.Model):
@@ -26,7 +49,9 @@ class Order(models.Model):
     tax = models.PositiveSmallIntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     discount = models.PositiveSmallIntegerField()
-    total_price = models.PositiveIntegerField()
+    total_price = models.DecimalField(
+        max_digits=15, decimal_places=2, editable=True, default=0
+    )
     is_delivered = models.BooleanField(default=False)
     shipping_address = models.CharField(max_length=255)
     status = models.CharField(max_length=35, choices=STATUSES)
