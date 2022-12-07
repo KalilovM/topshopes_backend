56be68923cac7a756451020c1c9b4905acd25f1b
KalilovM <marlenkalilov86@gmail.com>
2022-12-07 16:09:55 +0600

    Added cors policy

--- core/settings.py
+++ core/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "drf_yasg",
+    "corsheaders",
     # local apps
     "users.apps.UsersConfig",
     "shops.apps.ShopsConfig",
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
