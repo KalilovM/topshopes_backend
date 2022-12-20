8d8d69050eaad1433492705a67db68008e6ac044
KalilovM <marlenkalilov86@gmail.com>
2022-12-20 16:13:40 +0600

    Reformatted static pages logic Added static pages to admin

--- head/routers.py
+++ head/routers.py
@@ -8,8 +8,7 @@ from head.views import (
     AdminShopViewSet,
     AdminUsersViewSet,
     AdminPostViewSet,
-    AdminPageNavigationCategoryViewSet,
-    AdminPageNavigationViewSet,
+    AdminPageCategoryViewSet,
     AdminPageViewSet,
 )
 
@@ -21,6 +20,5 @@ router.register(r"brand/type", AdminBrandTypeViewSet)
 router.register(r"brand", AdminBrandViewSet)
 router.register(r"products", AdminProductViewSet)
 router.register(r"posts", AdminPostViewSet)
-router.register(r"pages/navigations/categories", AdminPageNavigationCategoryViewSet)
-router.register(r"pages/navigations", AdminPageNavigationViewSet)
+router.register(r"page/categories", AdminPageCategoryViewSet)
 router.register(r"pages", AdminPageViewSet)
