72f76aa1c52a7c88e0210a45f1e4df44d0fe7116
KalilovM <marlenkalilov86@gmail.com>
2022-12-21 12:50:00 +0600

    Added character field as id in page category

--- pages/models.py
+++ pages/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class PageCategory(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
     title = models.CharField(max_length=100)
 
     def __str__(self):
