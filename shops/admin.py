ccfcd14ec49d5daf62ac347e1ddf3c81b4b1f350
KalilovM <marlenkalilov86@gmail.com>
2022-12-26 12:09:13 +0600

    deleted product register in shop.admin.py

--- shops/admin.py
+++ shops/admin.py
@@ -4,24 +4,5 @@ from .models import (
     Shop,
 )
 
-from products.models import (
-    Size,
-    Color,
-    BrandType,
-    Image,
-    Category,
-    Brand,
-    Product,
-    Review,
-)
-
 admin.site.register(Link)
 admin.site.register(Shop)
-admin.site.register(Size)
-admin.site.register(Color)
-admin.site.register(BrandType)
-admin.site.register(Image)
-admin.site.register(Category)
-admin.site.register(Brand)
-admin.site.register(Product)
-admin.site.register(Review)
