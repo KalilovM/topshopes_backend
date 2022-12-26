8c035510b1e1d9239aec7e0ecff4dddbf844d422
KalilovM <marlenkalilov86@gmail.com>
2022-12-26 13:00:56 +0600

    Fixed fill command

--- orders/models.py
+++ orders/models.py
@@ -13,9 +13,13 @@ class OrderItem(models.Model):
     Contains one product information
     """
 
-    product_image = models.ImageField(upload_to=PathAndRename("orders/items/product/"))
-    product_name = models.CharField(max_length=100)
-    product_price = models.DecimalField(max_digits=10, decimal_places=2)
+    product_image = models.ImageField(
+        upload_to=PathAndRename("orders/items/product/"), blank=True, null=True
+    )
+    product_name = models.CharField(max_length=100, blank=True, null=True)
+    product_price = models.DecimalField(
+        max_digits=10, decimal_places=2, blank=True, null=True
+    )
     product_quantity = models.PositiveIntegerField()
     product_variant = models.ForeignKey(
         "products.ProductVariant", on_delete=models.CASCADE, related_name="order_items"
@@ -25,20 +29,20 @@ class OrderItem(models.Model):
     def __str__(self):
         return self.product_name
 
-    def change_order_total(self, isAdd: bool) -> None:
-        total_price = self.product_price * Decimal(self.product_quantity)
-        order = Order.objects.get(id=self.order.id)
-        total_price -= total_price * Decimal(order.discount) / 100
+    def fill_product_data(self) -> None:
+        self.product_name = self.product_variant.product.title
+        self.product_price = self.product_variant.price
+        self.product_image = self.product_variant.thumbnail
 
-        if isAdd:
-            order.total_price += total_price
+    def change_order_total(self, is_add: bool) -> None:
+        if is_add:
+            self.order.total_price += self.product_price * self.product_quantity
         else:
-            order.total_price -= total_price
-        order.total_price += total_price * Decimal(order.tax) / 100
-
-        order.save()
+            self.order.total_price -= self.product_price * self.product_quantity
+        self.order.save()
 
     def save(self, *args, **kwargs) -> None:
+        self.fill_product_data()
         self.change_order_total(True)
         return super().save(*args, **kwargs)
 
