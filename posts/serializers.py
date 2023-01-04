edc10ee69e1dee9fba6e155db11d055dbcf1d741
KalilovM <marlenkalilov86@gmail.com>
2023-01-04 12:09:58 +0600

    Deleted author from posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,11 +1,8 @@
 from rest_framework import serializers
 from posts.models import Post
-from rest_framework.serializers import Field
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author: Field = serializers.PrimaryKeyRelatedField(read_only=True)
-
     class Meta:
         model = Post
         fields = "__all__"
