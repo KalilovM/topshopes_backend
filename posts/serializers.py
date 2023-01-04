8729b6899424ba83da0af309c7e7970476699b36
KalilovM <marlenkalilov86@gmail.com>
2023-01-04 12:00:16 +0600

    Make author field autodetect in posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -3,6 +3,8 @@ from posts.models import Post
 
 
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.ReadOnlyField(source="author.username")
+
     class Meta:
         model = Post
         fields = "__all__"
