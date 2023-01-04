e5115ce665b4d73255e5cac942858359aa0065d4
KalilovM <marlenkalilov86@gmail.com>
2023-01-04 16:08:39 +0600

    Created SingleSliderSerializer

--- sliders/serializers.py
+++ sliders/serializers.py
@@ -1,14 +1,28 @@
 from rest_framework import serializers
 from .models import Slider, Slide
+from rest_framework.serializers import Field
 
 
 class SlideSerializer(serializers.ModelSerializer):
+    slider: Field = serializers.SlugRelatedField(
+        slug_field="slug", queryset=Slider.objects.all()
+    )
+
     class Meta:
         model = Slide
         fields = "__all__"
 
 
 class SliderSerializer(serializers.ModelSerializer):
+    slides = SlideSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Slider
+        fields = ["title", "slug"]
+
+
+class SingleSliderSerializer(serializers.ModelSerializer):
+
     slides = SlideSerializer(many=True, read_only=True)
 
     class Meta:
