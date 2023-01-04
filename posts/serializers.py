5fd99061286b7dacd959ae7911ada3144c16cd47
KalilovM <marlenkalilov86@gmail.com>
2023-01-04 12:08:10 +0600

    Post author changed to PrimaryRelated

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,10 @@
 from rest_framework import serializers
 from posts.models import Post
+from rest_framework.serializers import Field
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.ReadOnlyField()
+    author: Field = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Post
