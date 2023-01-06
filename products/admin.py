a5133ec7b777da97ac1519b64b19e11983afc5ba
KalilovM <marlenkalilov86@gmail.com>
2023-01-06 16:24:17 +0600

    Change Meta options on productvariant Remove field name from productvariant

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
-from .models import Product, Category, ProductVariant, Image, Brand, BrandType
+from .models import (
+    Product,
+    Category,
+    ProductVariant,
+    Image,
+    Brand,
+    BrandType,
+    ProductAttribute,
+    ProductAttributeValue,
+)
 
 
-for model in [Product, Category, ProductVariant, Image, Brand, BrandType]:
+for model in [Product, Category, ProductVariant, Image, Brand, BrandType, ProductAttribute, ProductAttributeValue]:
     admin.site.register(model)
