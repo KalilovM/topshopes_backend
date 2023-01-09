30cf098e9a2cd1e15163d3910fd1ee1665b7bcb1
KalilovM <marlenkalilov86@gmail.com>
2023-01-09 14:14:32 +0600

    Fixed slashes

--- pages/routers.py
+++ pages/routers.py
@@ -3,5 +3,5 @@ from rest_framework.routers import SimpleRouter
 from .views import PageCategoriesViewSet, PageViewSet
 
 router = SimpleRouter()
-router.register("pages/categories/", PageCategoriesViewSet, basename="page-category")
-router.register("pages/", PageViewSet, basename="page")
+router.register("pages/categories", PageCategoriesViewSet, basename="page-category")
+router.register("pages", PageViewSet, basename="page")
