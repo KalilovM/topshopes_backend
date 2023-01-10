19757033d4ff1f4233b0275986d81ab55eec2265
KalilovM <marlenkalilov86@gmail.com>
2023-01-10 13:15:40 +0600

    Added to admin panel attributes

--- attributes/admin.py
+++ attributes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Attribute, AttributeValue
+
+admin.site.register(Attribute)
+admin.site.register(Attribute, AttributeValue)
