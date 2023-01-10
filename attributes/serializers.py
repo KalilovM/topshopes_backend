11c01d10be5ca986073efa18b08d415bba3ac7ff
KalilovM <marlenkalilov86@gmail.com>
2023-01-10 13:58:56 +0600

    Added id to attribute serializer

--- attributes/serializers.py
+++ attributes/serializers.py
@@ -5,63 +5,6 @@ from products.models import Category, Product, ProductVariant
 from .models import Attribute, AttributeValue
 
 
-class AttributeReadSerializer(serializers.ModelSerializer):
-    """
-    Attribute return id and name
-    """
-
-    class Meta:
-        model = Attribute
-        fields = ["id", "name"]
-
-
-class AttributeValueSerializer(serializers.ModelSerializer):
-    """
-    Product attribute value serializer for read only
-    Return only name and product
-    """
-
-    attribute = serializers.SlugRelatedField(
-        slug_field="name", queryset=Attribute.objects.all()
-    )
-
-    class Meta:
-        model = AttributeValue
-        fields = ["id", "product_variant", "attribute", "value"]
-
-
-class CreateAttributeValueSerializer(serializers.ModelSerializer):
-    """
-    Product variant attribute value serializer for write only
-    Return only name and product
-    """
-
-    class Meta:
-        model = AttributeValue
-        fields = ["attribute", "value"]
-
-    def validate(self, data):
-        if AttributeValue.objects.filter(
-            attribute=data["attribute"], product_variant=self.context["product_variant"]
-        ).exists():
-            raise serializers.ValidationError(
-                {"detail": "Product attribute value already exists"}
-            )
-        return data
-
-    def create(self, validated_data):
-        product_variant = ProductVariant.objects.get(
-            id=self.context["product_variant"].id
-        )
-        if product_variant.product.shop.user != self.context["user"]:
-            raise serializers.ValidationError(
-                {"detail": "You are not allowed to create product attribute value"}
-            )
-        validated_data["product_variant"] = product_variant
-        product_attribute_value = AttributeValue.objects.create(**validated_data)
-        return product_attribute_value
-
-
 class CreateAttributeSerializer(serializers.ModelSerializer):
     """
     Product variant attribute serializer for write only
@@ -100,3 +43,48 @@ class AttributeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Attribute
         fields = ["id", "name"]
+
+
+class AttributeValueSerializer(serializers.ModelSerializer):
+    """
+    Product attribute value serializer for read only
+    Return only name and product
+    """
+
+    attribute = AttributeSerializer(read_only=True)
+
+    class Meta:
+        model = AttributeValue
+        fields = ["id", "product_variant", "attribute", "value"]
+
+
+class CreateAttributeValueSerializer(serializers.ModelSerializer):
+    """
+    Product variant attribute value serializer for write only
+    Return only name and product
+    """
+
+    class Meta:
+        model = AttributeValue
+        fields = ["attribute", "value"]
+
+    def validate(self, data):
+        if AttributeValue.objects.filter(
+            attribute=data["attribute"], product_variant=self.context["product_variant"]
+        ).exists():
+            raise serializers.ValidationError(
+                {"detail": "Product attribute value already exists"}
+            )
+        return data
+
+    def create(self, validated_data):
+        product_variant = ProductVariant.objects.get(
+            id=self.context["product_variant"].id
+        )
+        if product_variant.product.shop.user != self.context["user"]:
+            raise serializers.ValidationError(
+                {"detail": "You are not allowed to create product attribute value"}
+            )
+        validated_data["product_variant"] = product_variant
+        product_attribute_value = AttributeValue.objects.create(**validated_data)
+        return product_attribute_value
