03f839119e828c81d040b31097d93e6761375afe
KalilovM <marlenkalilov86@gmail.com>
2023-01-10 13:23:42 +0600

    Fixed bug

--- attributes/admin.py
+++ attributes/admin.py
@@ -3,4 +3,4 @@ from django.contrib import admin
 from .models import Attribute, AttributeValue
 
 admin.site.register(Attribute)
-admin.site.register(Attribute, AttributeValue)
+admin.site.register(AttributeValue)
