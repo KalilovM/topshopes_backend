dafe92dde6eee943af231bca81ec06f64336e987
kalilovm <marlenkalilov86@gmail.com>
2023-01-14 20:49:38 +0600

    Added django-redis Added caching settings Configured product buy services.py

--- products/services.py
+++ products/services.py
@@ -1,4 +1,6 @@
 from typing import Dict
+import redis
+from redis.exceptions import LockError
 
 from rest_framework import serializers
 
@@ -18,24 +20,35 @@ def buy_product(
     """
     Buy product service function
     """
-    if type(quantity) != int:
-        try:
-            quantity = int(quantity)
-        except ValueError:
-            raise serializers.ValidationError("Invalid quantity")
-    if product_variant.stock < quantity:
-        raise serializers.ValidationError("Not enough quantity")
-    product_variant.stock -= quantity
-    product_variant.save()
-    order_item = CreateOrderSerializer(
-        data={
-            "product_variant": product_variant.id,
-            "quantity": quantity,
-            "user": user,
-            "address": address,
-            "shop": shop,
-        }
-    )
-    order_item.is_valid(raise_exception=True)
-    order_item.save()
-    return order_item.data
+    r = redis.Redis()
+    # lock product variant quantity field
+    lock = r.lock(f'product_variant_{product_variant.id}_quantity', timeout=30)
+    try:
+        with lock.acquire():
+            if type(quantity) != int:
+                try:
+                    quantity = int(quantity)
+                except ValueError:
+                    raise serializers.ValidationError("Invalid quantity")
+            if product_variant.stock < quantity:
+                raise serializers.ValidationError("Not enough quantity")
+            product_variant.stock -= quantity
+            product_variant.save()
+            order_item = CreateOrderSerializer(
+                data={
+                    "product_variant": product_variant.id,
+                    "quantity": quantity,
+                    "user": user,
+                    "address": address,
+                    "shop": shop,
+                }
+            )
+            order_item.is_valid(raise_exception=True)
+            order_item.save()
+            return order_item.data
+
+    except LockError:
+        raise serializers.ValidationError("Lock error")
+    except serializers.ValidationError:
+        raise serializers.ValidationError("Validation error")
+
