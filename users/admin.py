1fae4a568049216424ae2aeb24e90141db444040
kalilovm <marlenkalilov86@gmail.com>
2023-01-19 15:56:14 +0600

    Registered application in admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Customer, Address
+from .models import Customer, Address, Application
 
 admin.site.register(Customer)
 admin.site.register(Address)
+admin.site.register(Application)
