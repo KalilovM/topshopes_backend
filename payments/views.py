ce7d939d9b9b45e35604f9a457cba9e544bb8e41
KalilovM <marlenkalilov86@gmail.com>
2023-01-28 13:12:08 +0600

    Fixed str error

--- payments/views.py
+++ payments/views.py
@@ -50,7 +50,7 @@ class AdminPaymentViewSet(viewsets.ModelViewSet):
     queryset = Payment.objects.all()
 
     def update(self, request, *args, **kwargs):
-        if request.data.get("is_verified"):
+        if request.data.get("is_verified") == "true":
             payment = self.get_object()
             payment.orders.update(status="paid")
         else:
