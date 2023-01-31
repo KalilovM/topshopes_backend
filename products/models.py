fca9dd5d8da980e805ce1cf888e91709e30062c8
KalilovM <marlenkalilov86@gmail.com>
2023-01-31 12:09:54 +0600

    Changed tax and discount price calc logic

--- products/models.py
+++ products/models.py
@@ -179,11 +179,11 @@ class ProductVariant(models.Model):
 
     def save(self, *args, **kwargs):
         if self.discount:
-            self.discount_price = self.price * Decimal(self.discount)/Decimal(100)
+            self.discount_price = self.price - (self.price * Decimal(self.discount)/Decimal(100))
         else:
             self.discount_price = None
 
-        self.overall_price = self.discount_price - (self.discount_price * self.product.category.tax/100)
+        self.overall_price = self.discount_price + (self.discount_price * self.product.category.tax/100)
 
         if self.stock == 0:
             self.status = "unavailable"
